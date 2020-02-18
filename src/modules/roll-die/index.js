226cebe1a83fd94d9f112b182c78ea7e05aba7a8
Graham De Young <grahamd0@gmail.com>
2020-02-17 19:34:35 -0600

    Removed redundant check in rollDie

--- src/modules/roll-die/index.js
+++ src/modules/roll-die/index.js
@@ -5,7 +5,7 @@
  * @return {int} The number rolled.
  */
 export default (sides, randFn = Math.random) => {
-  if (typeof sides !== 'number' || !Number.isInteger(sides)) {
+  if (!Number.isInteger(sides)) {
     throw new Error('rollDie must be called with an integer');
   }
 
