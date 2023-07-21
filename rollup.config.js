af9c6c50e7a66c5f15ed74ec4d779c3d48c28656
Graham De Young <github.com/alittletooquiet>
2023-07-20 22:55:15 -0500

    Updated ts config, fixed rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/dice-utils.umd.js',
